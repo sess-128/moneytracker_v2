82bb1a184d7828a39be1637b83091be655aace22
Nikon <102325439+MrNikaMilon@users.noreply.github.com>
2026-05-01 13:53:15 +0300

    Added api and response/request, added service

--- src/main/kotlin/ru/rrtyui/moneytracker/api/dto/CategoryResponseDto.kt
+++
@@ -1,6 +0,0 @@
-package ru.rrtyui.moneytracker.api.dto
-
-data class CategoryResponseDto(
-    val name: String,
-    val description: String,
-)
---
+++ src/main/kotlin/ru/rrtyui/moneytracker/api/dto/category/CategoryRequestDto.kt
@@ -0,0 +1,6 @@
+package ru.rrtyui.moneytracker.api.dto.category
+
+data class CategoryRequestDto(
+    val name: String,
+    val description: String
+)
